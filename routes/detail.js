eb17273768d536825b5dfa41c8fefa49cd86ab03
apolo <apollo423@protonmail.com>
2021-01-25 13:37:27 -0500

    0125_1

--- routes/detail.js
+++ routes/detail.js
@@ -69,43 +69,132 @@ router.post("/sendmail",async(req,res,next)=>{
         let cid = req.body.param.cid
         let uid = req.body.param.uid
         let carInfo = await Car.findOne({_id:cid})
-        let html = 
-        '<div style="text-align: center;margin:20px"><h2 style="font-weight: bold;">' + 
-        carInfo.make + 
-        '' + 
-        carInfo.model + 
-        '</h2></div><div style="display: flex;justify-content: space-between;margin: 20px"><div style="width: 50%;padding-right: 10px"><img style="width: 100%" src="' + 
-        carInfo.images[0] + 
-        '"></div><div style="width: 50%;padding-left: 10px"><div style="text-align: center;font-size: 25px"><p>About Vechile</p></div><div><div style="display: flex;justify-content: space-between;margin: 10px"><div>Price</div><div><h2 style="font-weight: bold; color: green;">'+ 
-        '$' + 
-        carInfo.price[0].USD + 
-        '</h2></div></div><div style="display: flex;justify-content: space-between;margin: 10px"><div>Date</div><div><h2 style="font-size: 18px;">' + 
-        carInfo.build_year + 
-        '</h2></div></div><div style="display: flex;justify-content: space-between;margin: 10px"><div>Odometer</div><div>' + 
-        carInfo.odometer + 
-        '</div></div><div style="display: flex;justify-content: space-between;margin: 10px"><div>Engine</div><div>'+
-        carInfo.engine_size + ' ' + carInfo.engine_size_unit + 
-        '</div></div><div style="display: flex;justify-content: space-between;margin: 10px"><div>Vin</div><div>' + 
-        carInfo.vin + 
-        '</div></div></div></div></div>'
-          /*
-          var transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-              user: 'admin@carsfromaustralia.com',
-              pass: '3a419kyrUR#UkK%l'
-            }
-          });
-          var mailOptions = {
-            from: 'carsfromaustralia@hotmail.com',
-            to: mail,
-            subject: 'CarInfo from CarSite!',
-            html: html
-          };
-          */
-          /** */
-          //console.log(currentCar)
-          /** */
+        let html = `
+        <html>
+            <style>
+              .carItem-left{
+                float: left;
+                width: 50%;
+                height: 30px;
+              }
+              .carItem-right{
+                float: right;
+                width: 50%;
+                
+                height: 30px;
+              }
+              p{
+                margin: 2px!important;
+              }
+            </style>
+          <body>
+            <div style="text-align: center;margin:5px">
+              <h2 style="font-weight: bold;font-size:30px">${carInfo.make} ${carInfo.model}</h2>
+            </div>
+            <table role="presentatio" cellspacing="1" cellpadding="5px" border="0px" style="width: 100%">
+              <tr>
+                <td style="width:50%">
+                  <img width="600" style="width: 100%" src="${carInfo.images[0]}"/>
+                </td>
+                <td style="width:50%">
+                  <div style="text-align: center;font-size: 25px">
+                    <p>About Vechile</p>
+                  </div>
+                  <div style="text-align: center;font-size: 12px;margin:10px">
+                    <p>${carInfo.description}</p>
+                  </div>
+                  <div style="text-align: center;font-size:15px">
+                  		<table style="text-align: center;width: 100%">
+                  			<tr>
+                  				<td>
+                        			<p>Price</p>
+                  				</td>
+                  				<td>
+                  					<p style="font-weight: bold; color: green;">$ ${carInfo.price[0].USD}</p>
+                  				</td>
+                  			</tr>
+                  			<tr>
+                  				<td>
+                        			<p>Shipment Price</p>
+                  				</td>
+                  				<td>
+                  					<p style="font-weight: bold; color: green;">$1200USD</p>
+                  				</td>
+                  			</tr>
+                  			<tr>
+                  				<td>
+                        			<p>Date</p>
+                  				</td>
+                  				<td>
+                  					<p>${carInfo.build_year}</p>
+                  				</td>
+                  			</tr>
+                  			<tr>
+                  				<td>
+                        			<p>BodyType</p>
+                  				</td>
+                  				<td>
+                  					<p>${carInfo.body_type}</p>
+                  				</td>
+                  			</tr>
+                  			<tr>
+                  				<td>
+                        			<p>Odometer</p>
+                  				</td>
+                  				<td>
+                  					<p>${carInfo.odometer}</p>
+                  				</td>
+                  			</tr>
+                  			<tr>
+                  				<td>
+                        			<p>Engine</p>
+                  				</td>
+                  				<td>
+                  					<p>${carInfo.engine_size} ${carInfo.engine_size_unit}</p>
+                  				</td>
+                  			</tr>
+                  			<tr>
+                  				<td>
+                        			<p>Location</p>
+                  				</td>
+                  				<td>
+                  					<p>${carInfo.location}</p>
+                  				</td>
+                  			</tr>
+                  			<tr>
+                  				<td>
+                        			<p>Seat</p>
+                  				</td>
+                  				<td>
+                  					<p>${carInfo.seat}</p>
+                  				</td>
+                  			</tr>
+                  			<tr>
+                  				<td>
+                        			<p>Door</p>
+                  				</td>
+                  				<td>
+                  					<p>${carInfo.door}</p>
+                  				</td>
+                  			</tr>
+                  			<tr>
+                  				<td>
+                        			<p>Vin</p>
+                  				</td>
+                  				<td>
+                  					<p>${carInfo.vin}</p>
+                  				</td>
+                  			</tr>
+
+                  		</table>
+                  	</div>
+                </td>
+              </tr>
+            </table>
+              
+          </body>
+        </html>
+        `
           let existUserInquiry = await Inquiry.findOne({
             user:uid,
             car:cid
